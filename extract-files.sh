LineageOS/android_device_motorola_sdm660-common

sdm660-common: Add a check before adding dependencies
Without a check, when performing a vendor blob regen it'll add another
dependency on top of any existing one

Change-Id: I9c7f17082fd850235c794d44cec3cda0c4b883ef

extract-files.sh
@@ -66,7 +66,9 @@ function blob_fixup() {
             ;;
         # memset shim
         vendor/bin/charge_only_mode)
-            "${PATCHELF}" --add-needed "libmemset_shim.so" "${2}"
+            for  LIBMEMSET_SHIM in $(grep -L "libmemset_shim.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libmemset_shim.so" "$LIBMEMSET_SHIM"
+            done
             ;;
         # Load wrapped shim
         vendor/lib64/libmdmcutback.so)
@@ -74,7 +76,9 @@ function blob_fixup() {
             ;;
         # Fix missing symbols
         vendor/lib64/libril-qc-hal-qmi.so)
-            "${PATCHELF}" --add-needed "libcutils_shim.so" "${2}"
+            for  LIBCUTILS_SHIM in $(grep -L "libcutils_shim.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libcutils_shim.so" "$LIBCUTILS_SHIM"
+            done
             ;;
     esac
 }

LineageOS/android_device_xiaomi_twolip

extract-files.sh
@@ -9,7 +9,9 @@
 function blob_fixup() {
     case "${1}" in
         vendor/lib/hw/camera.sdm660.so)
-            "${PATCHELF}" --add-needed "libcamera_sdm660_shim.so" "${2}"
+            for LIBCAMERA_SDM660_SHIM in $(grep -L "libcamera_sdm660_shim.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libcamera_sdm660_shim.so" "$LIBCAMERA_SDM660_SHIM"
+            done
             ;;
     esac
 }