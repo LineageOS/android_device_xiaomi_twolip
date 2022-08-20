e23b9bad2c5549ff7a6754bc126d6920c4406010
Davide Garberi <dade.garberi@gmail.com>
2022-08-20 04:04:12 +0200

    twolip: Nuke USB 1.0 basic
    Change-Id: I232e70dc5f7e50108778d0bb8a7d17bf54c7052d

--- device.mk
+++ device.mk
@@ -72,10 +72,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
-
 # Wifi
 PRODUCT_PACKAGES += \
     TulipWifiOverlay
